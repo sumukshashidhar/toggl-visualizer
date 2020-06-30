dd2545bb5d5510902e74216ef8fdbe9d70305a46
sumukshashidhar <sumukuuu@gmail.com>
2020-06-30 20:22:56 +0530

    added the init files

---
+++ main.py
@@ -0,0 +1,3 @@
+import src.datareader as datareader
+
+
--- src/datareader.py
+++ src/datareader.py
@@ -1,8 +1,20 @@
 import pandas as pd
 
-filepath = './../samples/sample.csv'
 
 
-df = pd.read_csv(filepath)
 
-print(df.head())
+
+filepath = './samples/sample.csv'
+
+
+
+
+class Reader:
+
+    def __init__(self, filepath):
+        self.df = pd.read_csv(filepath)
+    
+
+    def get_full_dataframe(self):
+        return self.df
+    
