27ca9934ed6f07426a37904802b531e75fa1e0af
Sumuk Shashidhar <sumukuuu@gmail.com>
2020-06-30 21:35:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import src.datareader as datareader
 import src.visualize as vs
+
+
 data = datareader.Reader(filepath='data/1.csv')
-
-
-print(data.get_full_dataframe())
-
 data.clean()
 data.export_cleaned()
 
 
 data2 = datareader.Reader(filepath='data/1--clean.csv')
-print(data2.get_full_dataframe())
-
-
 a = vs.Plot(data2.get_full_dataframe())
 a.pie_plot()
